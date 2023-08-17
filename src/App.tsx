9794f07b377011393589a26195784e0f704225a9
phai <pekopon_@live.fr>
2023-08-18 00:09:20 +0700

    edit new route

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import HomePage from "./page/homePage/HomePage";
+import LoginPage from "./page/loginPage/LoginPage";
+import ProfilePage from "./page/profilePage/ProfilePage";
+import { BrowserRouter , Routes, Route } from 'react-router-dom';
+import IndexPage from "./page/indexPage/indexPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App bg-secondary-subtle">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<IndexPage />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage/>} />
+          <Route path="/profile/:userId" element={<ProfilePage/>} />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
