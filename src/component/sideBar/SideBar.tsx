c310a4840a01d3e5f78b5f96e203e5861863c551
phai <pekopon_@live.fr>
2023-08-28 23:06:46 +0700

    [+] add function to profile page [e] re-format code in sidebar

--- src/component/sideBar/SideBar.tsx
+++ src/component/sideBar/SideBar.tsx
@@ -1,23 +1,104 @@
-import React from 'react'
-import './Sidebar.css'
+import React from "react";
+import "./Sidebar.css";
+import { useNavigate } from "react-router-dom";
+import { useAppSelector } from "../../redux/Store";
 
 function SideBar() {
+  const userData = useAppSelector((state) => state.auth.user);
+  const prefix_img_url = process.env.REACT_APP_PREFIX_URL_IMG;
+  const navigate = useNavigate();
+  const profilePicture = useAppSelector((state) => state.auth.mockIMG);
+
+  const profile = () => {
+    navigate(`/profile/${userData?._id}`);
+  };
+
   return (
-    <ul className='mt-3 ms-2 ' style={{padding: "unset", width:"250px"}}>
-      <li className='rounded p-2 sidebar-hover-color w-100 text-start lh-base'>Profile</li>
-      <li className='rounded p-2 sidebar-hover-color w-100 text-start lh-base'><i className="pe-2 fs-4 bi bi-people-fill" style={{color:"#1B74E4"}}></i>Friends</li>
-      <li className='rounded p-2 sidebar-hover-color w-100 text-start lh-base'><i className="pe-2 fs-4 bi bi-clock-fill" style={{color:"#1B74E4"}}></i>Memories</li>
-      <li className='rounded p-2 sidebar-hover-color w-100 text-start lh-base'><i className="pe-2 fs-4 bi bi-save2" style={{color:"#1B74E4"}}></i>Saved</li>
-      <li className='rounded p-2 sidebar-hover-color w-100 text-start lh-base'><i className="pe-2 fs-4 bi bi-collection" style={{color:"#1B74E4"}}></i>Groups</li>
-      <li className='rounded p-2 sidebar-hover-color w-100 text-start lh-base'><i className="pe-2 fs-4 bi bi-tv" style={{color:"#1B74E4"}}></i>Video</li>
-      <li className='rounded p-2 sidebar-hover-color w-100 text-start lh-base'><i className="pe-2 fs-4 bi bi-shop-window" style={{color:"#1B74E4"}}></i>Marketplace</li>
-      <li className='rounded p-2 sidebar-hover-color w-100 text-start lh-base'><i className="pe-2 fs-4 bi bi-postcard-heart" style={{color:"#1B74E4"}}></i>Feeds</li>
-      <li className='rounded p-2 sidebar-hover-color w-100 text-start lh-base'><i className="pe-2 fs-4 bi bi-calendar-event" style={{color:"red"}}></i>Events</li>
-      <li className='rounded p-2 sidebar-hover-color w-100 text-start lh-base'><i className="pe-2 fs-4 bi bi-bar-chart-line-fill" style={{color:"#1B74E4"}}></i>Ads Manager</li>
-      <li className='rounded p-2 sidebar-hover-color w-100 text-start lh-base'><i className="pe-2 fs-4 bi bi-bullseye" style={{color:"#1B74E4"}}></i>Crisis response</li>
-      <li className='rounded p-2 sidebar-hover-color w-100 text-start lh-base'><i className="pe-2 fs-4 bi bi-caret-down-fill"></i>See more</li>
+    <ul className="mt-3 ms-2 " style={{ padding: "unset", width: "175px" }}>
+      <li
+        className="rounded p-2 sidebar-hover-color w-100 text-start lh-base text-capitalize"
+        onClick={() => profile()}
+      >
+        <img
+          alt="profile"
+          className="rounded-circle border me-2"
+          src={
+            userData?.picturePath
+              ? prefix_img_url + userData?.picturePath
+              : profilePicture
+          }
+          style={{ width: "36px", height: "36px", objectFit: "cover" }}
+        />
+        {userData?.firstName} {userData?.lastName}
+      </li>
+      <li className="rounded p-2 sidebar-hover-color w-100 text-start lh-base">
+        <i
+          className="pe-2 fs-4 bi bi-people-fill"
+          style={{ color: "#1B74E4" }}
+        ></i>
+        Friends
+      </li>
+      <li className="rounded p-2 sidebar-hover-color w-100 text-start lh-base">
+        <i
+          className="pe-2 fs-4 bi bi-clock-fill"
+          style={{ color: "#1B74E4" }}
+        ></i>
+        Memories
+      </li>
+      <li className="rounded p-2 sidebar-hover-color w-100 text-start lh-base">
+        <i className="pe-2 fs-4 bi bi-save2" style={{ color: "#1B74E4" }}></i>
+        Saved
+      </li>
+      <li className="rounded p-2 sidebar-hover-color w-100 text-start lh-base">
+        <i
+          className="pe-2 fs-4 bi bi-collection"
+          style={{ color: "#1B74E4" }}
+        ></i>
+        Groups
+      </li>
+      <li className="rounded p-2 sidebar-hover-color w-100 text-start lh-base">
+        <i className="pe-2 fs-4 bi bi-tv" style={{ color: "#1B74E4" }}></i>Video
+      </li>
+      <li className="rounded p-2 sidebar-hover-color w-100 text-start lh-base">
+        <i
+          className="pe-2 fs-4 bi bi-shop-window"
+          style={{ color: "#1B74E4" }}
+        ></i>
+        Marketplace
+      </li>
+      <li className="rounded p-2 sidebar-hover-color w-100 text-start lh-base">
+        <i
+          className="pe-2 fs-4 bi bi-postcard-heart"
+          style={{ color: "#1B74E4" }}
+        ></i>
+        Feeds
+      </li>
+      <li className="rounded p-2 sidebar-hover-color w-100 text-start lh-base">
+        <i
+          className="pe-2 fs-4 bi bi-calendar-event"
+          style={{ color: "red" }}
+        ></i>
+        Events
+      </li>
+      <li className="rounded p-2 sidebar-hover-color w-100 text-start lh-base">
+        <i
+          className="pe-2 fs-4 bi bi-bar-chart-line-fill"
+          style={{ color: "#1B74E4" }}
+        ></i>
+        Ads Manager
+      </li>
+      <li className="rounded p-2 sidebar-hover-color w-100 text-start lh-base">
+        <i
+          className="pe-2 fs-4 bi bi-bullseye"
+          style={{ color: "#1B74E4" }}
+        ></i>
+        Crisis response
+      </li>
+      <li className="rounded p-2 sidebar-hover-color w-100 text-start lh-base">
+        <i className="pe-2 fs-4 bi bi-caret-down-fill"></i>See more
+      </li>
     </ul>
-  )
+  );
 }
 
-export default SideBar
+export default SideBar;
--- src/page/profilePage/ProfilePage.tsx
+++ src/page/profilePage/ProfilePage.tsx
@@ -1,28 +1,161 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import NavBar from '../../component/navBar/NavBar'
 import Post from '../../component/post/Post'
 import ProfileDetail from '../../component/ProfileDetail'
 import { useMediaQuery } from '../../utils/useMediaQuery';
+import axios from 'axios';
+import { AppDispatch, useAppSelector } from '../../redux/Store';
+import { PostType, UserType } from '../../type';
+import { useDispatch } from 'react-redux';
+import { setLogIn } from '../../redux/authSlice';
+import Loading from '../../component/Loading';
+import { useParams } from 'react-router-dom';
 
 function ProfilePage() {
   const isLaptop = useMediaQuery("(min-width: 1000px)");
+  const [authToken, setAuthToken] = useState<string | undefined>(undefined)
+  const userData = useAppSelector((state) => state.auth.user);
+  const [friendData,setFriendData] = useState<UserType | undefined>(undefined)
+  const [posts, setPosts] = useState<PostType[] | undefined>(undefined);
+  const [processing, setProcessing] = useState(false);
+  const dispatch = useDispatch<AppDispatch>();
+  const prefixURL = process.env.REACT_APP_PREFIX_URL;
+  const params = useParams()
+
+  useEffect(()=> {
+    const token = getUserToken();
+    setAuthToken(token);
+    if (token) {
+      getPosts(token);
+      getFriendDetail(token);
+      if(!userData){
+        getUserDetail(token)
+      }
+    }
+    // eslint-disable-next-line
+  },[])
+
+  const getUserToken = () => {
+    const token = sessionStorage.getItem("userToken")
+    if(token && token.length > 0){
+      return token
+    }else {
+      window.location.replace('/');
+    }
+  }
+
+  const getPosts = async (token: string) => {
+    setProcessing(true);
+    try {
+      const response = await axios.get(`${prefixURL}/posts/${params.userId}/posts`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      const data = response.data;
+      const sortData = sortPost(data)
+      setPosts(sortData);
+    } catch (err) {
+      console.error(err);
+    } finally {
+      setProcessing(false);
+    }
+  };
+
+  const getUserDetail =async (token:string) => {
+    setProcessing(true);
+    try {
+      const response = await axios.get(`${prefixURL}/users`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      const data = response.data;
+      dispatch(setLogIn(data));
+    } catch (err) {
+      console.error(err);
+    } finally {
+      setProcessing(false);
+    }
+  }
+
+  const getFriendDetail =async (token:string) => {
+    setProcessing(true);
+    try {
+      const response = await axios.get(`${prefixURL}/users/friend/${params.userId}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      });
+      const data = response.data;
+      setFriendData(data)
+    } catch (err) {
+      console.error(err);
+    } finally {
+      setProcessing(false);
+    }
+  }
+
+  const sortPost = (posts: PostType[]): PostType[] => {
+    return posts.slice().sort((a, b) => {
+      if (a.createdAt === undefined && b.createdAt === undefined) {
+        return 0;
+      }
+      if (a.createdAt === undefined) {
+        return 1;
+      }
+      if (b.createdAt === undefined) {
+        return -1;
+      }
+      return new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime();
+    });
+  };
+
+  if(!authToken){
+    return (<></>)
+  }
+  
   return (
     <>
       <NavBar />
+      <Loading isShow={processing} />
       <div className='container' style={{minHeight:"94vh"}}>
         <div className={`${isLaptop? "d-flex" : ""} pb-3 gap-3`}>
           <div className={`${isLaptop? "" : "m-auto"} w-100`} style={{ maxWidth:"490px"}}>
             <div style={{height:"75px"}} ></div>
-            <ProfileDetail />
+            <ProfileDetail 
+              dateOfBirth={friendData?.dateOfBirth || ''} 
+              firstName={friendData?.firstName || ''} 
+              lastName={friendData?.lastName || ''} 
+              email={friendData?.email || ''} 
+              password={''} 
+              occupation={friendData?.occupation || ''} 
+              location={friendData?.location || ''}
+              picturePath={friendData?.picturePath}
+              createdAt={friendData?.createdAt}
+            />
           </div>
           <div className='w-100'>
-            <Post />
-            <Post />
-            <Post />
-            <Post />
-            <Post />
-            <Post />
-            <Post />
+            {posts && posts.length > 0 ? (
+              posts.map((post) => (
+                <Post
+                  key={post._id}
+                  userId={post.userId}
+                  firstName={post.firstName}
+                  lastName={post.lastName}
+                  description={post.description}
+                  userPicturePath={post.userPicturePath}
+                  picturePath={post.picturePath}
+                  likes={post.likes}
+                  comments={post.comments}
+                  createdAt={post.createdAt}
+                />
+              ))
+            ) : (
+              <div className="d-flex justify-content-center align-items-center text-secondary" style={{height:"60vh"}}>
+                <p>No post yet</p>
+              </div>
+            )}
           </div>
         </div>
       </div>
